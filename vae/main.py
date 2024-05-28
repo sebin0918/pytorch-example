fd5f0226f3d1b370e6810e2a7c20a1343992c0e2
sebin <sebin0918@naver.com>
2024-05-28 11:12:44 +0900

    add pandas at vae/requirements.txt

--- vae/main.py
+++ vae/main.py
@@ -77,6 +77,7 @@ def loss_function(recon_x, x, mu, logvar):
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    # 아무 주석 추가 하기 (for commit test)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
     return BCE + KLD
